9cf9540a25e006a792cccdcef957828178c5ca92
Yoann Gern <yoann@yoanngern.ch>
2016-10-26 14:33:55 +0200

    update footer

--- wp-content/themes/iahm_2016/footer.php
+++ wp-content/themes/iahm_2016/footer.php
@@ -43,7 +43,7 @@
 					<li><a href="http://www.laguerison.org/presentation/vision">Notre vision</a></li>
 					<li><a href="http://www.laguerison.org/presentation/confession-de-foi">Confession de foi</a></li>
 					<li><a href="http://www.laguerison.org/faire-un-don">Faire un don</a></li>
-					<li><a href="http://www.laguerison.org/contact">Contactez-nous</a></li>
+					<!--<li><a href="http://www.laguerison.org/contact">Contactez-nous</a></li>-->
 				</ul>
 				<ul class="social"><h1>Suivre l’AIMG</h1>
 					<li><a href="https://www.facebook.com/iahm.international/">Facebook</a></li>
@@ -76,19 +76,19 @@
 		<div class="content">
 			<nav class="sitemap">
 				<ul class="activities"><h1>Activities</h1>
+					<!--
 					<li><a href="http://www.laguerison.org/activites/soiree-miracles-et-guerisons">Miracles and Healings
 							nights</a></li>
 					<li><a href="http://www.laguerison.org/activites/espaces-guerison/qu-est-ce-qu-un-espace-guerison">Healing
 							Rooms</a></li>
 					<li><a href="/institut">Healing Institute</a></li>
-					<li><a href="http://www.laguerison.org/activites/priere-par-telephone">Prayer Line</a></li>
-					<li><a href="http://www.laguerison.org/activites/demande-de-priere">Prayer Request</a></li>
+					<li><a href="http://www.laguerison.org/activites/priere-par-telephone">Prayer Line</a></li>-->
+					<li><a href="http://healing-ministries.org/en/prayer-request">Prayer Request</a></li>
 				</ul>
 				<ul class="events"><h1>Events</h1>
-					<li><a href="http://www.laguerison.org/soirees-miracles-guerisons">Miracles and Healings nights</a>
-					</li>
-					<li><a href="http://www.laguerison.org/soirees-miracles-guerisons/itemlist/category/19-conférences">IAHM
-							Conferences</a></li>
+					<!--<li><a href="http://www.laguerison.org/soirees-miracles-guerisons">Miracles and Healings nights</a>
+					</li>-->
+					<li><a href="http://healing-ministries.org/en/events/">upcoming events</a></li>
 					<li><a href="https://www.facebook.com/JLTministries/events/"
 					       target="_blank">Jean-Luc Trachsel’s itinerary</a></li>
 				</ul>
@@ -96,10 +96,10 @@
 					<li><a href="http://www.laguerison.org/presentation/connaitre-jesus/comment-devenir-un-chretien">To
 							Know
 							Jesus</a></li>
-					<li><a href="http://www.laguerison.org/medias/nos-videos">Our videos</a></li>
-					<li><a href="http://www.laguerison.org/medias/enseignements">Teachings</a></li>
-					<li><a href="http://www.laguerison.org/medias/temoignages">Testimonies</a></li>
-					<li><a href="http://www.laguerison.org/presentation/garder-sa-guerison">How To Keep Healing</a></li>
+					<li><a href="http://healing-ministries.org/en/resources/videos/">Our videos</a></li>
+					<li><a href="http://healing-ministries.org/en/resources/testimonies/">Testimonies</a></li>
+					<li><a href="http://healing-ministries.org/en/resources/how-to-keep-healing/">Teachings</a></li>
+					<li><a href="http://healing-ministries.org/en/resources/jesus/">How To Know Jesus</a></li>
 				</ul>
 				<ul class="join_us"><h1>Join us</h1>
 					<li><a href="/partner">IAHM Partner</a></li>
@@ -107,11 +107,12 @@
 					<li><a href="/volunteer">Volunteer</a></li>
 				</ul>
 				<ul class="about"><h1>About IAHM</h1>
-					<li><a href="http://www.laguerison.org/presentation/qui-sommes-nous">Who are we?</a></li>
-					<li><a href="http://www.laguerison.org/presentation/vision">Our vision</a></li>
-					<li><a href="http://www.laguerison.org/presentation/confession-de-foi">Statement of Faith</a></li>
-					<li><a href="http://www.laguerison.org/faire-un-don">Make a donation</a></li>
-					<li><a href="http://www.laguerison.org/contact">Contact us</a></li>
+					<li><a href="http://healing-ministries.org/en/iahm/vision/">Who are we?</a></li>
+					<li><a href="http://healing-ministries.org/en/iahm/vision/">Our vision</a></li>
+					<li><a href="http://healing-ministries.org/en/iahm/vision/">Our mission</a></li>
+					<li><a href="http://healing-ministries.org/en/iahm/vision/">Our purposes</a></li>
+					<li><a href="http://healing-ministries.org/en/partner/">Make a donation</a></li>
+					<!--<li><a href="http://www.laguerison.org/contact">Contact us</a></li>-->
 				</ul>
 				<ul class="social"><h1>Follow IAHM</h1>
 					<li><a href="https://www.facebook.com/iahm.international/">Facebook</a></li>

4ea38e53a377d4da9de004ff76270540788267eb
Yoann Gern <yoann@yoanngern.ch>
2016-10-26 12:10:01 +0200

    update head

--- wp-content/themes/iahm_2016/header.php
+++ wp-content/themes/iahm_2016/header.php
@@ -50,7 +50,16 @@
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/handlebars-intl/handlebars-intl.min.js"></script>
 	<script src="<?php echo get_stylesheet_directory_uri(); ?>/js/vendor/handlebars-intl/locale-data/fr.js"></script>
 
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
+		ga('create', 'UA-36835114-5', 'auto');
+		ga('send', 'pageview');
+
+	</script>
 
 </head>
 

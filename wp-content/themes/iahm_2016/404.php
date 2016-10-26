45ef59fffa11331f5731ede73b93caa677c11e1f
Yoann Gern <yoann@yoanngern.ch>
2016-10-26 15:10:00 +0200

    page 404

--- wp-content/themes/iahm_2016/404.php
+++ wp-content/themes/iahm_2016/404.php
@@ -14,13 +14,13 @@ get_header(); ?>
 
 			<section class="error-404 not-found">
 				<header class="page-header">
+					<p></p><p></p>
 					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'iahm_2016' ); ?></h1>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
 					<p><?php _e( 'It looks like nothing was found at this location.', 'iahm_2016' ); ?></p>
 
-					<?php get_search_form(); ?>
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 

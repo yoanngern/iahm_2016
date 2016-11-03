d81bf65f9140ef79445fc3b46c806d8774e59062
Yoann Gern <yoann@yoanngern.ch>
2016-11-03 12:24:40 +0100

    update footer nav fr

--- wp-content/themes/iahm_2016/footer.php
+++ wp-content/themes/iahm_2016/footer.php
@@ -9,40 +9,38 @@
 		<div class="content">
 			<nav class="sitemap">
 				<ul class="activities"><h1>Activités</h1>
-					<li><a href="http://www.laguerison.org/activites/soiree-miracles-et-guerisons">Soirées Miracles et
+					<li><a href="/evenements/calendar/">Soirées Miracles et
 							Guérisons</a></li>
-					<li><a href="http://www.laguerison.org/activites/espaces-guerison/qu-est-ce-qu-un-espace-guerison">Espaces
+					<li><a href="/priere/espaces-guerison/">Espaces
 							guérison</a></li>
-					<li><a href="/fr/institut">Institut de guérison</a></li>
-					<li><a href="http://www.laguerison.org/activites/priere-par-telephone">Ligne de prière</a></li>
-					<li><a href="http://www.laguerison.org/activites/demande-de-priere">Demande de prière</a></li>
+					<li><a href="/evenements/institut/presentation/">Institut de guérison</a></li>
+					<li><a href="/priere/telephone/"/priere/telephone/">Ligne de prière</a></li>
+					<li><a href="/priere/online/">Demande de prière</a></li>
 				</ul>
 				<ul class="events"><h1>Events</h1>
-					<li><a href="http://www.laguerison.org/soirees-miracles-guerisons">Soirées Miracles et Guérisons</a>
+					<li><a href="/evenements/calendar/">Soirées Miracles et Guérisons</a>
 					</li>
-					<li><a href="http://www.laguerison.org/soirees-miracles-guerisons/itemlist/category/19-conférences">Conférences
+					<li><a href="/evenements/netherlands/presentation/">Conférences
 							AIMG</a></li>
 					<li><a href="https://www.facebook.com/JLTministries/events/"
 					       target="_blank">Itinéraire de Jean-Luc Trachsel</a></li>
 				</ul>
 				<ul class="resources"><h1>Ressources</h1>
-					<li><a href="http://www.laguerison.org/presentation/connaitre-jesus/comment-devenir-un-chretien">Connaître
+					<li><a href="/ressources/jesus/">Connaître
 							Jésus</a></li>
-					<li><a href="http://www.laguerison.org/medias/nos-videos">Nos vidéos</a></li>
-					<li><a href="http://www.laguerison.org/medias/enseignements">Enseignements</a></li>
-					<li><a href="http://www.laguerison.org/medias/temoignages">Témoignages</a></li>
-					<li><a href="http://www.laguerison.org/presentation/garder-sa-guerison">Garder sa guérison</a></li>
+					<li><a href="/ressources/videos/">Nos vidéos</a></li>
+					<li><a href="/ressources/temoignages/">Témoignages</a></li>
+					<li><a href="/ressources/garder-sa-guerison/">Garder sa guérison</a></li>
 				</ul>
 				<ul class="join_us"><h1>Rejoignez-nous</h1>
-					<li><a href="/fr/partner">Partenaire AIMG</a></li>
-					<li><a href="/fr/intercessor">Intercesseur</a></li>
-					<li><a href="/fr/volunteer">Bénévole</a></li>
+					<li><a href="/partenaire/">Partenaire AIMG</a></li>
+					<li><a href="/aimg/intercesseur/">Intercesseur</a></li>
+					<li><a href="/aimg/benevole/">Bénévole</a></li>
 				</ul>
 				<ul class="about"><h1>A propos de l’AIMG</h1>
-					<li><a href="http://www.laguerison.org/presentation/qui-sommes-nous">Qui sommes-nous ?</a></li>
-					<li><a href="http://www.laguerison.org/presentation/vision">Notre vision</a></li>
-					<li><a href="http://www.laguerison.org/presentation/confession-de-foi">Confession de foi</a></li>
-					<li><a href="http://www.laguerison.org/faire-un-don">Faire un don</a></li>
+					<li><a href="/aimg/vision/">Notre vision</a></li>
+					<li><a href="/aimg/confession-de-foi/">Confession de foi</a></li>
+					<li><a href="/don/">Faire un don</a></li>
 					<!--<li><a href="http://www.laguerison.org/contact">Contactez-nous</a></li>-->
 				</ul>
 				<ul class="social"><h1>Suivre l’AIMG</h1>
@@ -51,7 +49,7 @@
 				</ul>
 			</nav>
 			<div class="info">
-				<div class="logo"><a id="logo_aimg" target="_blank" href="http://www.laguerison.org"></a></div>
+				<div class="logo"><a id="logo_aimg" target="_blank" href="http://healing-ministries.org/fr/"></a></div>
 				<div class="address"><h1>AIMG</h1>
 					<p>Route du Flon 28<br/>
 						1610 Oron-La-Ville<br/>

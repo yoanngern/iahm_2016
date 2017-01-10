d1157d19042c5f05931eb6ce3861d6a11bb9ff35
Yoann Gern <yoann@yoanngern.ch>
2017-01-10 17:33:45 +0100

    update favicon

--- wp-content/themes/iahm_2016/header.php
+++ wp-content/themes/iahm_2016/header.php
@@ -28,7 +28,8 @@
 	<link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-16x16.png">
 	<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/manifest.json">
-	<meta name="msapplication-TileColor" content="#ffffff">
+	<link rel="mask-icon" href="favicon_hd.svg" color="#B5191D">
+	<meta name="msapplication-TileColor" content="#B5191D">
 	<meta name="msapplication-TileImage" content="<?php echo get_stylesheet_directory_uri(); ?>/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
 
